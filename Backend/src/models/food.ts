ee4d6c559f4d447af9a4b8393e8ec92237248a42
creed28 <hristo.zagorliev44@gmail.com>
2024-03-28 16:27:03 +0200

    Create food model

---
+++ Backend/src/models/food.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const foodSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  kCal: { type: Number, required: true },
+  protein: {type: Number, required: true },
+  fat: {type: Number, required: true },
+  carbs: {type: Number, required: true }
+});
+
+type Food = mongoose.InferSchemaType<typeof foodSchema>;
+
+export default mongoose.model<Food>("Food", foodSchema);
+ 
