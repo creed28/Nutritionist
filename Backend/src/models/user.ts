2d0cd4fe91332d0f1ee1b485439ea766b7b1e8fc
creed28 <hristo.zagorliev44@gmail.com>
2024-03-28 16:24:54 +0200

    Fix user model

---
+++ Backend/src/models/user.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true, select: false },
+  password: { type: String, required: true, select: false },
+});
+
+type User = mongoose.InferSchemaType<typeof userSchema>;
+
+export default mongoose.model<User>("User", userSchema);
