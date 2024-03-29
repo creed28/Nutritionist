945fb6802c2f43d41ca0149356350b6822b5db4c
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2024-03-29 14:15:59 +0200

    Create food interface and fetch foods from the backend

---
+++ Frontend/src/models/food.ts
@@ -0,0 +1,8 @@
+export interface Food {
+  _id: string,
+  name: string,
+  kCal: number,
+  protein: number,
+  fat: number,
+  carbs: number
+}
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,9 +1,28 @@
+import { Food } from "../models/food";
+import { useEffect, useState } from "react";
+import axios from '../api/axios';
+
 const Home = () => {
+  const [foods, setFoods] = useState<Food[]>([]);
+
+  useEffect(() => {
+    async function loadFoods() {
+      try {
+        const res = await axios.get("/foods");
+        setFoods(res.data);
+      } catch (error) {
+          console.error(error);
+      }
+    }
+  
+    loadFoods();
+  }, []);  
+
   return (
     <section>
-      Home
+      {JSON.stringify(foods)}
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
