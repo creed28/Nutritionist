6734eccc5e9853ace074a6d5bb67d4b8d60cb9cd
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2024-03-29 11:23:59 +0200

    Export interfaces from separate folder

---
+++ Backend/src/interfaces/food.ts
@@ -0,0 +1,11 @@
+export interface UpdateFoodParams {
+  foodId: string,
+}
+
+export interface FoodBody {
+  name?: string,
+  kCal?: number,
+  protein?: number,
+  fat?: number,
+  carbs?: number
+}
--- Backend/src/models/food.ts
+++ Backend/src/models/food.ts
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const foodSchema = new mongoose.Schema({
   name: { type: String, required: true },
   kCal: { type: Number, required: true },
-  protein: {type: Number, required: true },
-  fat: {type: Number, required: true },
-  carbs: {type: Number, required: true }
+  protein: { type: Number, required: true },
+  fat: { type: Number, required: true },
+  carbs: { type: Number, required: true }
 });
 
 type Food = mongoose.InferSchemaType<typeof foodSchema>;
