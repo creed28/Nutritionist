b2474050d2d8d03a2c3028011c83075ce4d2d258
creed28 <hristo.zagorliev44@gmail.com>
2024-04-03 20:28:49 +0300

    Add login and register requests

--- Frontend/src/components/auth/LoginForm.tsx
+++ Frontend/src/components/auth/LoginForm.tsx
@@ -1,17 +1,50 @@
 import { Input } from "../ui/Input";
 import { Button } from "../ui/Button";
+import { useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
+import axios from "../../api/axios";
+import useFoodsContext from "../../hooks/useFoodsContext";
+
+interface LoginInput {
+  username: string,
+  password: string
+}
 
 const LoginForm = () => {
+  const { 
+    register, 
+    handleSubmit, 
+    formState :{ 
+      isSubmitting 
+    }} = useForm<LoginInput>();
+
+  const { setAuth } = useAuth();
+  const { loadFoodsInTable } = useFoodsContext();
+
+  const onSubmit = async (input: LoginInput) => {
+    try {
+      const res = await axios.post("/users/login", input);
+      setAuth(res.data);
+      loadFoodsInTable();
+    } catch (error) {
+        console.error(error);
+    }
+  }
+
   return (
-    <form className="mb-2 flex flex-col mx-6">
+    <form 
+      className="mb-2 flex flex-col mx-6"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className="w-full flex items-center gap-y-4 flex-col">
         <Input
-          placeholder="Name"
-          id="name"
+          placeholder="Username"
+          id="username"
           className="formInput w-[90%] mx-6"
           type="text"
           required
           autoFocus
+          {...register("username")}
         />
         <Input
           placeholder="Password"
@@ -19,10 +52,17 @@ const LoginForm = () => {
           className="formInput w-[90%] mx-6"
           type="password"
           required
+          {...register("password")}
         />
       </div>
       <div className="mt-4 mx-4">
-        <Button className='w-full' type="submit">Sign in</Button>
+        <Button 
+          className='w-full' 
+          type="submit"
+          disabled={isSubmitting}
+        >
+            Login
+        </Button>
       </div>
     </form>
   );
--- Frontend/src/components/auth/RegisterForm.tsx
+++ Frontend/src/components/auth/RegisterForm.tsx
@@ -1,9 +1,42 @@
 import { Input } from "../ui/Input";
 import { Button } from "../ui/Button";
+import { useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
+import axios from "../../api/axios";
+import useFoodsContext from "../../hooks/useFoodsContext";
+
+interface RegisterInput {
+  username: string,
+  email: string,
+  password: string
+}
 
 const LoginForm = () => {
+  const { 
+    register, 
+    handleSubmit, 
+    formState :{ 
+      isSubmitting 
+    }} = useForm<RegisterInput>();
+
+  const { setAuth } = useAuth();
+  const { loadFoodsInTable } = useFoodsContext();
+
+  const onSubmit = async (input: RegisterInput) => {
+    try {
+      const res = await axios.post("/users/signup", input);
+      setAuth(res.data);
+      loadFoodsInTable();
+    } catch (error) {
+        console.error(error);
+    }
+  }
+
   return (
-    <form className="mb-2 flex flex-col mx-6">
+    <form 
+      className="mb-2 flex flex-col mx-6"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className="w-full flex items-center gap-y-4 flex-col">
         <Input
           placeholder="Name"
@@ -12,6 +45,7 @@ const LoginForm = () => {
           type="text"
           required
           autoFocus
+          {...register("username")}
         />
         <Input
           placeholder="Email"
@@ -19,6 +53,7 @@ const LoginForm = () => {
           className="formInput w-[90%] mx-6"
           type="email"
           required
+          {...register("email")}
         />
         <Input
           placeholder="Password"
@@ -26,11 +61,16 @@ const LoginForm = () => {
           className="formInput w-[90%] mx-6"
           type="password"
           required
+          {...register("password")}
         />
       </div>
-      <div className="mt-4 mx-4">
-        <Button className='w-full' type="submit">Register</Button>
-      </div>
+      <Button 
+          className='w-full' 
+          type="submit"
+          disabled={isSubmitting}
+        >
+            Sign Up
+        </Button>
     </form>
   );
 }
