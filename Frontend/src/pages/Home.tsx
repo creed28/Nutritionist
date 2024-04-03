23978ad0868903b456a00d20bee3fded7b12120a
creed28 <hristo.zagorliev44@gmail.com>
2024-04-03 18:29:09 +0300

    Load search results if input is not empty

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -2,14 +2,21 @@ import TotalCard from "../components/TotalCard";
 import FoodList from "../components/FoodList";
 import SearchBar from "../components/search/SearchBar";
 import SearchResultList from "../components/search/SearchResultList";
+import useFoodsContext from "../hooks/useFoodsContext";
+
 const Home = () => {
-  
+  const { searchInput } = useFoodsContext();
+
   return (
-    <main className="bg-[#bee9b0] pt-10 flex flex-col items-center">
-      <SearchBar />
-      <SearchResultList />
-      <TotalCard />
-      <FoodList />
+    <main className="bg-[#bee9b0] pt-10 flex flex-col gap-y-10 items-center">
+      <div className="flex flex-col w-full items-center relative">
+        <SearchBar />
+        {searchInput.length > 0 && <SearchResultList />}
+      </div>
+      <div className="flex flex-col items-center gap-y-2">
+        <TotalCard />
+        <FoodList />
+      </div>
     </main>
   );
 }
