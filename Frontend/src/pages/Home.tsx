adb79d957537b897819ba3520c1eb5dadf031acb
creed28 <hristo.zagorliev44@gmail.com>
2024-04-03 20:26:34 +0300

    Show message if there is no authenticated user

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -3,9 +3,11 @@ import FoodList from "../components/FoodList";
 import SearchBar from "../components/search/SearchBar";
 import SearchResultList from "../components/search/SearchResultList";
 import useFoodsContext from "../hooks/useFoodsContext";
+import useAuth from "@/hooks/useAuth";
 
 const Home = () => {
   const { searchInput } = useFoodsContext();
+  const { auth } = useAuth();
 
   return (
     <main className="bg-[#bee9b0] pt-10 flex flex-col gap-y-10 items-center">
@@ -15,7 +17,12 @@ const Home = () => {
       </div>
       <div className="flex flex-col items-center gap-y-2">
         <TotalCard />
-        <FoodList />
+        {auth ? 
+          <FoodList /> 
+        : 
+          <p className="font-Rounded1c">
+            Login to use <span className="font-Pacifico">Nutritionist</span>
+          </p>}
       </div>
     </main>
   );
