c918ab37e82944ff131e4a6636e937faf0d6fce7
creed28 <hristo.zagorliev44@gmail.com>
2024-04-03 18:31:04 +0300

    Restyle footer

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="text-center text-sm py-8 bg-[#bee9b0]" id="main-footer">
+    <footer className="text-center text-sm py-8 bg-[#bee9b0] absolute bottom-0 w-full" id="main-footer">
       &copy; {new Date().getFullYear()} Created by 
       <span className="font-Pacifico"> Hristo Zagorliev</span>. 
       All Rights Reserved.
