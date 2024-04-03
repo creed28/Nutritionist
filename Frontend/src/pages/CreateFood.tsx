7c1823bdc817126edeaf706492a354e2ce1271e1
creed28 <hristo.zagorliev44@gmail.com>
2024-04-03 18:29:24 +0300

    Center create food

--- Frontend/src/pages/CreateFood.tsx
+++ Frontend/src/pages/CreateFood.tsx
@@ -2,7 +2,7 @@ import CreateFoodForm from "../components/CreateFoodForm";
 
 const CreateFood = () => {
   return (
-    <main className="bg-[#bee9b0] flex justify-center items-center">
+    <main className="bg-[#bee9b0] flex justify-center items-center h-[calc(100vh-148.5px)]">
       <CreateFoodForm />
     </main>
   );
