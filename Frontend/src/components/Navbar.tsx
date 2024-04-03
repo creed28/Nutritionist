c4b3988c6cc420d09cb75cc7e928689a560e53d6
creed28 <hristo.zagorliev44@gmail.com>
2024-04-03 20:27:09 +0300

    Create logged in and logged off views

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,7 +1,13 @@
 import { useLocation } from "react-router-dom";
 import AuthDialog from "./auth/AuthDialog";
+import useAuth from "../hooks/useAuth";
+import useLogout from "../hooks/useLogout";
+import { Button } from "./ui/Button";
 
 const Navbar = () => {
+  const { auth } = useAuth();
+  const logout  = useLogout();
+
   const location = useLocation();
 
   const isActive = (path : string) => {
@@ -12,20 +18,33 @@ const Navbar = () => {
     <nav>
       <ul className="flex items-center gap-x-4 text-sm font-semibold">
         <div className="flex items-center gap-x-4">
-          <li>
-            <a href="/" className={`${isActive("/") && "activeLink"}`}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/create-food" className={`${isActive("/create-food") && "activeLink"}`}>
-              Create Food
-            </a>
-          </li>
+          {auth ? 
+            <>
+              <li>
+                <a href="/" className={`${isActive("/") && "activeLink"}`}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="/create-food" className={`${isActive("/create-food") && "activeLink"}`}>
+                  Create Food
+                </a>
+              </li>
+              <li>
+                <p>{auth.username}</p>
+              </li>
+              <li>
+                <Button variant="link" size="link" onClick={logout}>
+                  Logout
+                </Button>
+              </li>
+            </>
+          :
+            <li>
+              <AuthDialog />
+            </li>
+          }
         </div>
-        <li>
-          <AuthDialog />
-        </li>
       </ul>
     </nav>
   );
