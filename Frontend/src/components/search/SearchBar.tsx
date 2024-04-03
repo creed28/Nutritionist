a0b67b12c1672a92fb58e70584f3b2c6d4182a13
creed28 <hristo.zagorliev44@gmail.com>
2024-04-03 18:30:05 +0300

    Remove outline from input

--- Frontend/src/components/search/SearchBar.tsx
+++ Frontend/src/components/search/SearchBar.tsx
@@ -3,7 +3,6 @@ import { Input } from "../ui/Input";
 import useFoodsContext from "../../hooks/useFoodsContext";
 
 const SearchBar = () => {
-
   const { searchInput, setSearchInput, fetchSearchData } = useFoodsContext();
 
   const handleChange = (value: string) => {
@@ -15,7 +14,7 @@ const SearchBar = () => {
     <section className="flex bg-white rounded-lg h-[2.5rem] px-4 items-center w-[30%]">
       <FaSearch />
       <Input 
-        className="border-none h-full text-[1.25rem] ml-1 focus:outline-none"
+        className="border-none h-full text-[1.25rem] ml-1"
         type="text" 
         placeholder="Type to search..."
         value={searchInput}
